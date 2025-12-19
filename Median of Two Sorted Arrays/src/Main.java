11a8f3bb2f1ee3a39be2b34bd59a5dc7bedfd8cd
Aimkey <phamminhkiet24@gmail.com>
2025-12-19 10:55:36 +0700

    Not optimized, but get the job done

--- Median of Two Sorted Arrays/src/Main.java
+++ Median of Two Sorted Arrays/src/Main.java
@@ -1,51 +1,49 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new App().run();
-    }
-}
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
-class App {
-    public void run() {
-        int[] nums1 = new int[] { 1, 2 };
-        int[] nums2 = new int[] { 3, 4 };
-        System.out.println(Solution.findMedianSortedArrays(nums1, nums2));
-    }
+class Main {
 
+    public static void main(String[] args) {
+        int[] nums1 = new int[] { 1,3 };
+        int[] nums2 = new int[] { 2 };
+        System.out.println(new Solution().findMedianSortedArrays(nums1, nums2));
+    }
 }
 
 class Solution {
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i = 0;
-        int j = 0;
-        int lengthNum1 = nums1.length;
-        int lengthNum2 = nums2.length;
-        int k = (int) (lengthNum1 + lengthNum2) / 2;
-        int temp = 0;
-        double result = 0;
-        for (int count = 0; count <= k; count++) {
-            System.out.println("Result: " + result + ", temp: " + temp + ", count: " + count);
-            if (i == lengthNum1)
-                i--;
-            if (j == lengthNum2)
-                j--;
-            if (nums1[i] < nums2[j]) {
-                if (count == k - 1) {
-                    temp = nums1[i];
-                }
-                i++;
-                result = nums1[i];
-            } else {
-                if (count == k - 1) {
-                    temp = nums2[i];
-                }
-                j++;
-                result = nums2[j];
-            }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Merge the two array first
+        Integer[] merged = new Integer[nums1.length + nums2.length];
+        int index = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            merged[index] = nums1[i];
+            index++;
         }
-        if ((lengthNum1 + lengthNum2) * 1.0 / 2 > k) {
-            return result;
+
+        for (int i = 0; i < nums2.length; i++) {
+            merged[index] = nums2[i];
+            index++;
+        }
+        // Sort them
+        Arrays.sort(merged, (a, b) -> a - b);
+        // Find the median
+        double median = 0;
+        if (merged.length % 2 == 0) {
+            // median = sum of the 2 num in the middle / 2 (Ex: 4 => num1 + num2) 1 2 3 4 5 6
+            median = (merged[merged.length / 2] + merged[merged.length / 2 - 1]) * 1.0 / 2;
         } else {
-            return (result + temp) / 2;
+            // median = the middle number (Ex: 5 nums => 5 / 2 = 2 (The second num))
+            median = merged[merged.length / 2];
         }
+        printArray(merged);
+        return median;
+    }
+
+    public <T> void printArray(T[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
